8283954994c3ad397376afc09ed39409fe489f9d
jlesniak2018 <joseph.f.lesniak@gmail.com>
2015-09-04 11:59:43 -0400

    Person1 completed

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -30,16 +30,26 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  Array chars[] = input.toCharArray();
+      String rotated = "";
+      for (int i = input.length(); i >= 0; i--) {
+          if (i == 1) {
+              rotated = rotated + chars[0];
+          } else if (i == 0) {
+              rotated = rotated + chars[1];
+          } else {
+              rotated = chars[i] + rotated;
+          }
+      }
+	  return rotated;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
