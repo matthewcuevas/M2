7c5aa5d0dd489f51effd6134f818ec2d1c183308
jlesniak2018 <joseph.f.lesniak@gmail.com>
2015-09-04 14:22:47 -0400

    Person1.java updated

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,8 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  String firstTwoLetters = input.subString(0, 2);
-      String rest = input.subString(2);
+	  String firstTwoLetters = input.substring(0, 2);
+      String rest = input.substring(2);
 	  return rest + firstTwoLetters;
 	}
 
